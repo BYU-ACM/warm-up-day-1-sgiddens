dc1ecb2adc3f1288d27ca1057f36156a1f9e1090
Spencer Giddens <giddens2spencer@gmail.com>
2017-01-10 12:49:02 -0700

    Added file for Newton's Method

--- sol.py
+++ sol.py
@@ -1,13 +1,13 @@
 def Newtons_Method(func, func_prime, x_0, iters=100, tol=1e-5):
   """A direct implementation of Newton's Method
-     (For sanity assume that func and func_prime define there own division correctly,
+     (For sanity assume that func and func_prime define their own division correctly,
      that is, don't cast anything to a float)
      params: func: a function,
-             fuc_prime: its direvative
-             x_0: an itial guess (required)
-             iters:number of iterations to cap too (optional, default 100)
+             fuc_prime: its derivative
+             x_0: an initial guess (required)
+             iters: number of iterations to cap too (optional, default 100)
              tol: acceptable distance from 0 (optional, default 1e-5)
      returns: a root(int) if found
               None(none-type) else
   """
-  pass
+    
